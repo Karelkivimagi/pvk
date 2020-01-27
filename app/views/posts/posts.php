b9c28b8fb4bebfa6c9bd0d85733831c1b9148ad6
Karel Kivimägi <karel.kivimagi@khk.ee>
2020-01-27 12:36:02 +0200

    Eemaldatud test failid raamistiku jaoks

--- app/views/posts/posts.php
+++ app/views/posts/posts.php
@@ -3,6 +3,10 @@ require_once '../app/views/inc/header.php';
 ?>
     <h1><?php echo $data['title'];?></h1>
     <p><?php echo $data['content'];?></p>
+<?php foreach($data['posts'] as $post):?>
+    <h3><?php echo $post->title; ?></h3>
+    <p><?php echo $post->content; ?></p>
+<?php endforeach ?>
 <?php
 require_once '../app/views/inc/footer.php';
 ?>
